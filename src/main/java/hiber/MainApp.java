97a79790def217dbdd5b553584e39b47ad672b10
AlpinistV <spasovecvadim@gmail.com>
2022-04-04 15:58:29 +0300

    No comments

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,49 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        Car car1 = new Car("LADA", 1);
+        userService.add(user1);
+
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        Car car2 = new Car("Mersedes", 2);
+        userService.add(user2);
+
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        Car car3 = new Car("KAMAZ", 3);
+        userService.add(user3);
+
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+        Car car4 = new Car("Volvo", 4);
+        userService.add(user4);
+
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println((user.getCar() == null)? "У пользователя нет машины " : "Car: " +  user.getCar().toString());
+            System.out.println();
+        }
+
+        context.close();
+    }
+}
